95f65e16f3cd2767ddd9261702907369cd9f3f04
TobiasMu <tobiasmuldager@gmail.com>
2025-08-24 20:43:26 +0200

    veri nice

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -53,4 +53,16 @@ return {
     },
   },
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
 }
